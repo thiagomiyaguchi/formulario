73e88b8db4de4596aebb2589e3c90c41fa1a6cae
thiagomiyaguchi <thiagomiyaguchi@hotmail.com>
2023-02-23 16:07:40 -0300

    atualizando estados dos inputs

--- src/components/CampoTexto/index.jsx
+++ src/components/CampoTexto/index.jsx
@@ -1,10 +1,16 @@
-import './CampoTexto.css';
+import "./CampoTexto.css";
 
 export default function CampoTexto(props) {
   return (
-    <div className='campo-texto'>
+    <div className="campo-texto">
       <label htmlFor={props.name}>{props.label}</label>
-      <input type={'text'} id={props.name} name={props.name} />
+      <input
+        type={"text"}
+        id={props.name}
+        name={props.name}
+        value={props.valor}
+        onChange={props.fn}
+      />
     </div>
   );
 }
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,35 +1,56 @@
-import './Formulario.css';
-import CampoTexto from '../CampoTexto';
-import ListaSuspensa from '../ListaSuspensa';
-import Checkbox from '../Checkbox';
-import Botao from '../Botao';
-import turmas from '../../turmas.json';
-import jogosPorPlataformas from '../../jogosPorPlataforma.json';
-import { useState } from 'react';
+import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Checkbox from "../Checkbox";
+import Botao from "../Botao";
+import turmas from "../../turmas.json";
+import jogosPorPlataformas from "../../jogosPorPlataforma.json";
+import { useState } from "react";
 
 export default function Formulario() {
-  const [plataforma, setplataforma] = useState('');
+  const [plataforma, setplataforma] = useState("");
+  const [nome, setNome] = useState("");
+  const [userName, setUserName] = useState("");
 
   const plataformas = jogosPorPlataformas.map((item) => item.plataforma);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+  };
+
   return (
-    <form className='formulario'>
-      <CampoTexto name='nome' label='Nome:' />
+    <form className="formulario" onSubmit={handleSubmit}>
+      <CampoTexto
+        name="nome"
+        label="Nome:"
+        valor={nome}
+        fn={(e) => {
+          setNome(e.target.value);
+        }}
+      />
       <ListaSuspensa
         itens={turmas}
-        label='Turma:'
-        placeholder='Selecione a sua turma'
-        listarJogos={(event) => console.log('oi')}
+        label="Turma:"
+        placeholder="Selecione a sua turma"
+        listarJogos={(event) => console.log("oi")}
       />
       <ListaSuspensa
         itens={plataformas}
-        label='Plataforma:'
-        placeholder='Selecione a sua plataforma'
+        label="Plataforma:"
+        placeholder="Selecione a sua plataforma"
         listarJogos={(event) => {
           setplataforma(event.target.value);
         }}
       />
-      <CampoTexto name='userName' label='User Name:' />
+      <CampoTexto
+        name="userName"
+        label="User Name:"
+        valor={userName}
+        fn={(e) => {
+          setUserName(e.target.value);
+        }}
+      />
       <Checkbox itens={jogosPorPlataformas} plataforma={plataforma} />
       <Botao />
     </form>
